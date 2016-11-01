b7a62c9b82cac10ec7eff0593f98e1a1f88039be
Kevin S. Miller <ksmiller99@gmail.com>
2016-11-01 11:04:46 -0400

    Commit before test deploy

---
+++ cOOKie/OokDevice.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+using System.IO;
+
+namespace cOOKie
+{
+    public class OokDevice
+    {
+        /// <summary>The edge of the PWM dataBits that has a constant period</summary>
+        public enum SyncEdges { Rising, Falling, Unknown}
+
+        public string name { get; private set; }
+
+        /// <summary>0's and 1's or ""; Start dataBits may have a unique time delay before the data dataBits</summary>
+        public string startBits { get; private set; }
+
+        /// <summary>The amount of time in ms between the last start bit and first databit</summary>
+        public double startBitPad_ms { get; private set; }
+        
+        /// <summary>The number of data dataBits in a PWM word</summary>
+        public int nDataBits { get; private set; }
+
+        /// <summary>The pulse width in ms of the high portion of the narrow bit</summary>
+        public double narrowWidth_ms { get; private set; }
+
+        /// <summary>The pulse width in ms of the high portion of the wide bit</summary>
+        public double wideWidth_ms { get; private set; }
+
+        /// <summary>The period for every data bit from SyncEdges to the next SyncEdges</summary>
+        public double bitPeriod_ms { get; private set; }
+
+        /// <summary>Determines if dataBits are synced on rising aor falling edge</summary>
+        public SyncEdges syncEdge { get; private set; }
+
+        /// <summary>The amount of time in ms before the first data bit (or start bit if existing)</summary>
+        public double wordPad_ms { get; private set; }
+
+        /// <summary>True if saved. This flag is managed by client</summary>
+        public bool isSaved { get; set; }
+
+        /// <summary>True if contains valid data. Flag set by this.validate(). Client is responsible for checking before using device</summary>
+        public bool isValid { get; private set; }
+
+        /// <summary>Set by this.validate(). Contains list of validtion errors or ""</summary>
+        public string errors { get; private set; }
+
+        public OokDevice(string name,
+                         string startBits,
+                         double startBitPad_ms,
+                         int nDataBits,
+                         double narrowWidth_ms,
+                         double wideWidth_ms,
+                         double bitPeriod_ms,
+                         SyncEdges syncEdge,
+                         double wordPad_ms)
+        {
+            string msg;
+            if (!setName(name, out msg))
+            { throw new ArgumentException(msg, "name"); }
+
+            if (!setStartBits(startBits, out msg))
+            { throw new ArgumentException(msg, "startBits"); }
+
+            if (!setStartBitPad(startBitPad_ms, out msg))
+            { throw new ArgumentException(msg, "startBitPad_ms"); }
+
+            if (!setNDataBits(nDataBits, out msg))
+            { throw new ArgumentException(msg, "nDataBits"); }
+
+            if (!setNarrowWidth_ms(narrowWidth_ms, out msg))
+            { throw new ArgumentException(msg, "narrowWidth_ms"); }
+
+            if (!setWideWidth_ms(wideWidth_ms, out msg))
+            { throw new ArgumentException(msg, "wideWidth_ms"); }
+
+            if (!setBitPeriod_ms(bitPeriod_ms, out msg))
+            { throw new ArgumentException(msg, "bitPeriod_ms"); }
+
+            if (!setSyncEdge(syncEdge, out msg))
+            { throw new ArgumentException(msg, "syncEdge"); }
+
+            if (!setWordPad_ms(wordPad_ms, out msg))
+            
+            this.isSaved = false;
+            this.isValid = false;
+            this.errors = "unvalidated";
+        }// end constructor
+
+        //empty constructor
+        //public OokDevice() { isValid = isSaved = false; }
+
+        public bool setName(string nm, out string msg)
+        {
+            bool ok;
+            if ((nm.IndexOfAny(Path.GetInvalidFileNameChars()) == -1) && (nm.IndexOf(" ") == -1))
+            {
+                name = nm;
+                ok = true;
+                msg = "";
+            }
+            else
+            {
+                ok = false;
+                msg = "Name contains invalid characters";
+            }
+            return ok;
+        }
+
+        public bool setStartBits(string sb, out string msg)
+        {
+            bool ok;
+            if (sb.Replace("1", "").Replace("0", "").Length == 0)
+            {
+                startBits = sb;
+                msg = "";
+                ok = true;
+            }
+            else
+            {
+                msg = "Start Bits can only contain 1's and 0's";
+                ok = false;
+            }
+                startBits = sb;
+            
+            return ok;
+        }
+
+        public bool setStartBitPad(double sbp, out string msg)
+        {
+            bool ok;
+            if (sbp >= 0 && sbp <= 1000)
+            {
+                startBitPad_ms = sbp;
+                ok = true;
+                msg = "";
+            }
+            else
+            {
+                msg = "Start bit pad must be beween 0 and 1000 ms";
+                ok = false;
+            }
+            return ok;
+        }
+
+        public bool setNDataBits(int ndb, out string msg)
+        {
+            bool ok;
+            if (ndb > 0 && ndb <= 1000)
+            {
+                nDataBits = ndb;
+                ok = true;
+                msg = "";
+            }
+            else 
+            {
+                msg = "Number of data bits must be between 1 and 1000";
+                ok = false;
+            }
+            return ok;
+        }
+
+        public bool setNarrowWidth_ms(double nw, out string msg)
+        {
+            bool ok;
+            if (nw > 0)
+            {
+                narrowWidth_ms = nw;
+                ok = true;
+                msg = "";
+            }
+            else
+            {
+                msg = "Narrow width must be > 0 and < wide width and < bit width";
+                ok = false;
+            }
+            return ok;
+        }
+
+        public bool setWideWidth_ms(double ww, out string msg)
+        {
+            bool ok;
+            if (ww > 0)
+            {
+                wideWidth_ms = ww;
+                ok = true;
+                msg = "";
+            }
+            else
+            {
+                msg = "Narrow width must be > 0 and < wide width and < bit width";
+                ok = false;
+            }
+            return ok;
+        }
+
+        //todo: handle blank wideWidth
+        public bool setBitPeriod_ms(double bp, out string msg) 
+        {
+            bool ok;
+            if (bp > 0)
+            {
+                bitPeriod_ms = bp;
+                ok = true;
+                msg = "";
+            }
+            else
+            {
+                ok = false;
+                msg = "Bit period must be > 0 and > wideWidth_ms";
+            }
+            return ok;
+        }
+
+        //todo: are there invalid selections?
+        public bool setSyncEdge(SyncEdges se, out string msg)
+        {
+            msg = "";
+            syncEdge = se;
+            return true;
+        }
+
+        //todo:  define max wordpad 
+        public bool setWordPad_ms(double wp, out string msg)
+        {
+            bool ok;
+            if (wp > 0)
+            {
+                wordPad_ms = wp;
+                ok = true;
+                msg = "";
+            }
+            else
+            {
+                msg = "Word pad must be >= 0 ms";
+                ok = false;
+            }
+            return ok;
+        }
+
+        public Int16[] makeWordSignal(UInt32 samplerate, string dataBits, int repeat, out string msg)
+        {
+            msg = "";
+            if(dataBits.Length != nDataBits){
+                msg = "Wrong number of bits";
+                return null;
+            }
+
+            if (dataBits.Count(f => f == '0') + dataBits.Count(f => f == '1') != dataBits.Length)
+            {
+                msg = "Only '1' and '0' allowed in word bits";
+                return null;
+            }
+
+            // srp = samplerate period
+            double srp = 1.0 / samplerate;
+
+            // spb = samples per bit
+            int spb = Convert.ToInt32((bitPeriod_ms / 1000) * samplerate);
+
+            // spw = samples per wide pulse
+            int spw =  Convert.ToInt32((wideWidth_ms / 1000) * samplerate);
+
+            // spn = samples per narrow pulse
+            int spn =  Convert.ToInt32((narrowWidth_ms / 1000) * samplerate);
+
+            // padN = padding of narrow pulse to get bit width
+            //int padN = spb - spn;
+
+            // padW = padding of wide pulse to get bit width
+            //int padW = spb - spw;
+
+
+
+            // Create narrow and wide dataBits to copy into signal
+            Int16[] narrowBit = new Int16[spb * 2];
+            Int16[] wideBit = new Int16[spb * 2];
+            
+            // maximum modulation set to 80%
+            double maxMod = 2047 * .8;
+            
+            if (syncEdge == SyncEdges.Falling)
+            {
+                // Put pulse on back end of bit
+                Array.Copy(Enumerable.Repeat((Int16)maxMod, spn * 2).ToArray(), 0, narrowBit, ((spb - spn) * 2), (spn * 2));
+                Array.Copy(Enumerable.Repeat((Int16)maxMod, spw * 2).ToArray(), 0, wideBit, ((spb - spw) * 2), (spw * 2));
+            }
+            else
+            {
+                // Put pulse on front end of bit
+                Array.Copy(Enumerable.Repeat((Int16)maxMod, spn * 2).ToArray(), 0, narrowBit, 0, (spn * 2));
+                Array.Copy(Enumerable.Repeat((Int16)maxMod, spw * 2).ToArray(), 0, wideBit, 0, (spw * 2));
+            }
+
+            // Calculate total number of samples:
+            // wordpad + (number of start dataBits * samples per bit) + startbit delay + 
+            // (number of data dataBits * samples per bit) + wordpad
+            
+            //length of one word incuding wordpad and start bits
+            Int32 nSamples = Convert.ToInt32(
+                             ((wordPad_ms / 1000) * samplerate) +
+                             (startBits.Length * spb) +
+                             ((startBitPad_ms / 1000) * samplerate) +
+                             (nDataBits * spb));
+
+            //multiply by the number of times word is repeated
+            nSamples *= repeat;
+
+            //add another wordpad at end of signal
+            nSamples += (Int32)((wordPad_ms / 1000) * samplerate);
+
+            // create array to hold signal - interleaved I and Q values: 2 array cell per sample 
+            Int16[] signal = Enumerable.Repeat((Int16)0,nSamples * 2).ToArray();
+            
+            // idx is pointer to current location in signal
+            // each sample is 2 interleaved 16-bit words (I and Q) 
+            int idx = Convert.ToInt32(((wordPad_ms / 1000) * samplerate) * 2);
+            
+            // ensure idx is on an even number
+            idx = (idx % 2 == 0) ? idx : idx + 1;
+
+            for (int r = 0; r < repeat; ++r)
+            {
+                //add start bits
+                foreach (char c in startBits)
+                {
+                    // narrow bit
+                    if (c == '0')
+                    {
+                        Array.Copy(narrowBit, 0, signal, idx, narrowBit.Length);
+                        idx += narrowBit.Length;
+                    }
+                    // wide bit
+                    else
+                    {
+                        Array.Copy(narrowBit, 0, signal, idx, wideBit.Length);
+                        idx += wideBit.Length;
+                    }
+                }
+
+                // Add space between start bits and first data bit
+                idx += Convert.ToInt32(((startBitPad_ms / 1000) * samplerate) * 2);
+
+                //add data dataBits
+                foreach (char c in dataBits)
+                {
+                    // narrow bit
+                    if (c == '0')
+                    {
+                        Array.Copy(narrowBit, 0, signal, idx, narrowBit.Length);
+                        idx += narrowBit.Length;
+                    }
+                    // wide bit
+                    else
+                    {
+                        Array.Copy(wideBit, 0, signal, idx, wideBit.Length);
+                        idx += wideBit.Length;
+                    }
+                }
+
+                idx += Convert.ToInt32(((wordPad_ms / 1000) * samplerate) * 2);
+            }// end repeat
+
+            return signal;
+   
+        }// makeWordSignal
+
+        public bool validate(out string msg)
+        {
+            msg = "";
+            
+            if(String.IsNullOrEmpty(name) || (name.IndexOfAny(Path.GetInvalidFileNameChars()) != -1) || (name.IndexOf(" ") != -1))
+                {msg+="name has invalid characters\n";}
+
+            return (msg == "");
+        }
+
+    }// end of class
+}
---
+++ cOOKie/PwmWord.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace cOOKie
+{
+    class PwmWord
+    {
+        public List<Bit> allBits;
+        public List<Bit> dataBits;
+        public List<Bit> startBits;
+        public List<Bin> bins;
+
+        //public enum SyncEdge{Rising, Falling, Unknown}
+
+        public UInt32 sampleRate            { get; set; }
+        public double avgNarrowWidth        { get; set; }
+        public double narrowStDev           { get; set; }
+        public double avgWideWidth          { get; set; }
+        public double wideStDev             { get; set; }
+        public double avgRisePeriod         { get; set; }
+        public double riseStDev             { get; set; }
+        public double avgFallPeriod         { get; set; }
+        public double fallStDev             { get; set; }
+        public OokDevice.SyncEdges syncEdge { get; private set; }
+        public string dataContents          { get; set; }
+        public string sbContents            { get; private set; }
+        public int startBitsN               { get; set; }
+        public int startBitWidth            { get; set; }
+        public int startBitDelay            { get; set; } //# of samples between last start bit and first data bit
+        public double interWordDelay        { get; set; } //# of samples between last data bit and start bit of next word
+
+        public PwmWord()
+        {
+            allBits = new List<Bit>();
+            bins = new List<Bin>();
+        }
+
+        /// <summary>
+        /// Analyzes the signal and populates the PwmWord properties. The signal should start just before start bit, 
+        /// and end just after next start bit.
+        /// </summary>
+        /// <param name="signal">double[,] from MATLAB</param>
+        /// <param name="sr">Sample Rate in Hz</param>
+        /// <param name="tolerance">% of time difference to be considered valid</param>
+        /// <returns></returns>
+        public bool analyzeSignal(double[,] signal, UInt32 sr, uint tolerance, out string msg)
+        {
+            this.sampleRate = sr;
+            msg = "";
+            //examine signal at middle level
+            double threshold = (signal.Cast<double>().ToList().Max() + signal.Cast<double>().ToList().Min())/2;
+
+            double lastLevel = 0; // used to determine direction when threshold is crossed
+
+            //examine all dataBits and place into allBits
+            for (int i = 1; i <= signal.Length-1; ++i)
+            {
+                double val = signal[i, 0];
+                if (lastLevel > threshold && val < threshold)
+                {
+                    //falling - last bit on list should have fall time and fall period blank
+                    allBits.Last().fallTime = i;
+                    if (allBits.Count > 1)
+                    {
+                        allBits.Last().fallPeriod = i - allBits[allBits.Count - 2].fallTime;
+                    }
+
+                    //calculate pulse width and bin it
+                    int pulseWidth = allBits.Last().fallTime - allBits.Last().riseTime;
+                    allBits.Last().pulseWidth = pulseWidth;
+                    bool found = false;
+                    foreach (Bin bin in bins)
+                    {
+                        //if pulsewidth is within the tolerance for this bin, then add it to bin
+                        if (Math.Abs(pulseWidth - bin.width) < (bin.width * (tolerance / 100.0)))
+                        {
+                            ++bin.count;
+                            found = true;
+                            allBits.Last().binName = bin.width;
+                            break;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        bins.Add(new Bin(pulseWidth));
+                        allBits.Last().binName = pulseWidth;
+                    }
+
+                }
+
+                if (lastLevel < threshold && val > threshold)
+                {
+                    //rising - last bit on list should be complete = add new bit
+                    allBits.Add(new Bit(allBits.Count+1));
+                    allBits.Last().riseTime = i;
+                    if (allBits.Count > 1)
+                    {
+                        allBits.Last().risePeriod = i - allBits[allBits.Count - 2].riseTime;
+                    }
+  
+                }
+
+                lastLevel = val;
+            }//end for
+
+            if (allBits.Count == 0) 
+            {
+                msg = "No bits found. Check your selections";
+                return false;
+            }
+
+            if (bins.Count != 2)
+            {
+               msg ="Not binary PWM - " + bins.Count.ToString() + " bit widths found.";
+                return false;
+            }
+
+            //calculate avg rise period including start dataBits
+            avgRisePeriod = (from bit in allBits select bit.risePeriod).Average();
+
+            // find and cout start bits
+            if (!findStartBits())
+            {
+                msg="Error in findStartBits()";
+                return false;
+            }
+
+            startBits = new List<Bit>();
+            startBits.AddRange(allBits.GetRange(0, startBitsN));
+
+            //separate data dataBits from start dataBits at beginning and end of signal
+            dataBits = new List<Bit>();
+            dataBits.AddRange(allBits.GetRange(startBitsN,allBits.Count-startBitsN-1));
+            
+            //get # of samples between last start bit and first databit, the remove first databit periods
+            dataBits[0].risePeriod = 0;
+            dataBits[0].fallPeriod = 0;
+
+            if (!calcBinValues(out msg))
+            {
+                return false;
+            }
+
+            double minBinName = (from bin in bins select bin.width).Min();
+            double maxBinName = (from bin in bins select bin.width).Max();
+
+            avgNarrowWidth = (from bit in dataBits where bit.binName == minBinName select bit.pulseWidth).Average();
+            avgWideWidth = (from bit in dataBits where bit.binName == maxBinName select bit.pulseWidth).Average();
+            
+            narrowStDev = stdDeviation(from bit in dataBits where bit.binName == minBinName select bit.pulseWidth);
+            wideStDev = stdDeviation(from bit in dataBits where bit.binName == maxBinName select bit.pulseWidth);
+            
+            avgRisePeriod = (from bit in dataBits where bit.bitPos > (startBitsN + 1) select bit.risePeriod).Average();
+            riseStDev = stdDeviation(dataBits
+                 .Where(bit => bit.bitPos > startBitsN+1)
+                 .Select(bit => bit.risePeriod)
+                 .ToList());
+            avgFallPeriod = (from bit in dataBits where bit.bitPos > (startBitsN + 1) select bit.fallPeriod).Average();
+            fallStDev = stdDeviation(dataBits
+                 .Where(bit => bit.bitPos > startBitsN + 1)
+                 .Select(bit => bit.fallPeriod)
+                 .ToList());    
+
+            // set syncEdge = must be a magnitude difference
+            if(riseStDev > 10 * fallStDev)
+            {
+                syncEdge = OokDevice.SyncEdges.Falling;
+                startBitDelay = Math.Max(dataBits[0].fallTime - startBits.Last().fallTime - Convert.ToInt32(avgFallPeriod),0);
+                interWordDelay = allBits.Last().fallTime - dataBits.Last().fallTime - avgFallPeriod;
+            }
+            else if(fallStDev > 10 * riseStDev)
+            {
+                syncEdge = OokDevice.SyncEdges.Rising;
+                startBitDelay = dataBits[0].riseTime - startBits.Last().riseTime - Convert.ToInt32(avgRisePeriod);
+                interWordDelay = allBits.Last().riseTime - dataBits.Last().riseTime - avgRisePeriod;
+            }
+            else
+            {
+                syncEdge = OokDevice.SyncEdges.Unknown;
+                startBitDelay = 0;
+            }
+
+           // set pwmWord dataContents
+            dataContents = "";
+            foreach (Bit bit in dataBits)
+            {
+                if (bit.binName == minBinName)
+                {
+                    dataContents += "0";
+                }
+                else
+                {
+                    if (bit.binName == maxBinName)
+                    {
+                        dataContents += "1";
+                    }
+                    else
+                    {
+                        dataContents += "x"; //more than 2 bins
+                    }
+                }
+            }
+
+            // set start bit dataContents
+            sbContents = "";
+            foreach (Bit bit in startBits)
+            {
+                if (bit.binName == minBinName)
+                {
+                    sbContents += "0";
+                }
+                else
+                {
+                    if (bit.binName == maxBinName)
+                    {
+                        sbContents += "1";
+                    }
+                    else
+                    {
+                        sbContents += "x"; //more than 2 bins
+                    }
+                }
+            }
+
+            //temp write dataBits to csv for excel double-checking
+            String csv = string.Join(",", (from bit in allBits select new { bit.binName, bit.riseTime, bit.fallTime }));
+            File.WriteAllText(@"c:\temp\ook.csv", csv);
+
+            calcBitDeviations();
+
+            return true;
+        }// end fill bins
+
+        protected double stdDeviation(IEnumerable<int> values)
+        {
+            double avg = values.Average();
+            double sum = values.Sum(d => Math.Pow(d-avg,2));
+            return(Math.Sqrt((sum) / (values.Count() - 1)));
+        }
+
+        /// <summary>
+        /// Calculate and store the standard deviation for each bin
+        /// </summary>
+        protected bool calcBinValues(out string msg)
+        {
+            msg = "";
+            bool ok = true;
+            IEnumerable<int> values;
+            foreach (Bin bin in bins)
+            {
+                values = from bit in dataBits
+                         where bit.binName == bin.width
+                         select bit.pulseWidth;
+
+                if (values.Count() > 0)
+                {
+                    bin.avgWidth = values.Average();
+                    bin.stdDev = stdDeviation(values);
+                }
+                else
+                {
+                    msg = "Not Binary PWM - One or more bins are empty";
+                    ok = false;
+                }
+            }
+
+            return ok;
+        }
+
+        private void calcBitDeviations()
+        {
+            foreach (Bit bit in dataBits)
+            {
+                //pw deviation from bin standard deviation
+                double stDevPw = (from bin in bins where bin.width == bit.binName select bin.stdDev).First();
+                bit.pwDeviation = Math.Abs(stDevPw - bit.pulseWidth);
+
+            }
+        }
+
+        /// <summary>
+        /// searches bit list to find one of more dataBits at the beginning of a word
+        /// seperated from the rest of the word
+        /// </summary>
+        private bool findStartBits()
+        {
+            bool ok = false;
+            startBitsN = 0;
+            Bit bit;
+            //start with second bit because first does not have a period
+            for (int i = 1; i < allBits.Count; ++i )
+            {
+                
+                bit = allBits[i];
+                if ((bit.risePeriod) > avgRisePeriod * 1.5)
+                {
+                    startBitsN = i;
+                    ok = true;
+                    break;
+                }
+            }
+
+            //if start bit count = number of bits in word, no startbit
+            
+
+            //if start(s) bit not found to have larger period, assume first bit is start bit
+            if (!ok || startBitsN == (allBits.Count-1))
+            {
+                startBitsN = 1;
+                ok = true;
+            }
+
+            return ok;
+        }
+
+    }   
+
+    class Bin
+    {
+        public int width { get; set; }      //pulse width in samples, also serves as name of bin
+        public int count { get; set; }      //number of pulses with widths within 10% of width
+        public double avgWidth { get; set; }//average width of bin
+        public double stdDev { get; set; }  //standard deviation
+
+        public Bin(int pw)
+        {
+            this.width = pw;
+            this.count = 1;
+        }
+    }
+
+    class Bit
+    {
+        public int binName { get; set; }        //bin name (bin.width) to which bit belongs
+        public int bitPos { get; set; }         //position of bit within word
+        public int riseTime { get; set; }       //sample number of rising edge
+        public int fallTime { get; set; }       //sample number of falling edge
+        public int pulseWidth { get; set; }     //fallTime - riseTime
+        public int risePeriod { get; set; }     //number of samples since previous bit's riseTime
+        public int fallPeriod { get; set; }     //number of samples since previous bi's fallTime
+        public double pwDeviation { get; set; } //pulse width distance from standard deviation for it's bin
+        public double rpDeviation { get; set; } //rise period distance from standard deviaiton
+        public double fpDeviation { get; set; } //fall period distance from deviation
+
+        public Bit(int pos)
+        {
+            bitPos = pos;
+        }
+    }
+}
